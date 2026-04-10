d12180ce0154a5c62b07d69270b0b4eefb23c9b8
technical-ysnlc <technical.ysnlc@gmail.com>
2026-04-10 15:42:03 +0800

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,13 +38,13 @@ Rails.application.configure do
 config.action_mailer.delivery_method = :smtp
 
 config.action_mailer.smtp_settings = {
-  address: ENV['SMTP_ADDRESS'],
-  port: 587,
-  user_name: ENV['SMTP_USERNAME'],
-  password: ENV['SMTP_PASSWORD'],
-  authentication: :login,
-  enable_starttls_auto: true,
-  domain: ENV['SMTP_DOMAIN'] || ENV['APP_HOST']
+  address: ENV["SMTP_ADDRESS"],
+  port: ENV["SMTP_PORT"],
+  domain: ENV["SMTP_DOMAIN"],
+  user_name: ENV["SMTP_USERNAME"],
+  password: ENV["SMTP_PASSWORD"],
+  authentication: ENV["SMTP_AUTHENTICATION"] || :login,
+  enable_starttls_auto: ENV["SMTP_ENABLE_STARTTLS_AUTO"] == "true"
 }
 
 config.action_mailer.default_url_options = {
